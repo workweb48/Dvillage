8e1593610d5d6f4851d8018f95148488d4083ccd
workweb48 <workweb48@gmail.com>
2019-11-04 22:55:03 +0300

    add  stiticremove dir

--- src/myweb/settings.py
+++ src/myweb/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
