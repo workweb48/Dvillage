3b06cfdb170ca26bdcf19d2144783b57d3e9e6f1
workweb48 <57195923+workweb48@users.noreply.github.com>
2019-11-05 22:17:35 +0300

    Update urls.py

--- myweb/urls.py
+++ myweb/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', include('users.url')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
